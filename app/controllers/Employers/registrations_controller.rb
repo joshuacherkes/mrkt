a3880b3c034ae59a4e07e759f40284a2b5e26a07
Joshua Cherkes <joshuacherkes@gmail.com>
2013-08-09 15:23:19 -0400

    changed file names

--- app/controllers/Employers/registrations_controller.rb
+++ app/controllers/Employers/registrations_controller.rb
@@ -1,14 +1,14 @@
 class Employers::RegistrationsController < Devise::RegistrationsController
   def new
     @employer = Employer.new
-    render "employer/registrations/new"
+    render "employers/registrations/new"
   end
 
-    def after_sign_up_path_for(employer)
-      new_job_url
-    end
-    
-    def after_sign_in_path_for(employer)
+  def after_sign_up_path_for(employer)
+    new_job_url
+  end
+  
+  def after_sign_in_path_for(employer)
     jobs_url
   end
 
